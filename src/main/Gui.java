32512aa7e2a3f976e9be697fa40ad7d916ba7b1e
rkg18 <rkg18@zips.uakron.edu>
2019-01-10 21:24:26 -0500

    Add window components and exit button

--- src/main/Gui.java
+++ src/main/Gui.java
@@ -1,22 +1,34 @@
 package main;
 
 import java.awt.*;
+import java.awt.event.*; 
 
-public class Gui extends Frame {
+public class Gui extends Frame implements ActionListener {
+	TextField tf;
+	
 	Gui()
 	{
-		Button testButton = new Button("Click me!");
 		
-		testButton.setBounds(50,50,50,50);
 		
-		add(testButton);
+		//create components  
+		tf=new TextField();  
+		tf.setBounds(60,50,170,20);  
+		Button b=new Button("click me");  
+		b.setBounds(100,120,80,30);  
+		  
+		// Register Listener Objects
+		b.addActionListener(this); //passing current instance  
+		addWindowListener(new WindowAdapter(){public void windowClosing(WindowEvent e){dispose();}});  
 		
-		setSize(500,300);
-		
-		setTitle("University of Akron Meal Calculator");
-		
-		setLayout(new FlowLayout());
-		
-		setVisible(true);
+		//add components and set size, layout and visibility  
+		add(b);add(tf);  
+		setSize(300,300);  
+		setLayout(null);  
+		setVisible(true);  
 	}
+	
+	public void actionPerformed(ActionEvent e) {
+		tf.setText("Welcome!");
+	}
+		
 }
